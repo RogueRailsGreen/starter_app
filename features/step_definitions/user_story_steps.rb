a98e98b58907e37e93b709e1c5948e5a52a35dba
Nathan Wiebe & Vyki Englert <pair+nwiebe+vykster@pivotallabs.com>
2013-10-24 17:55:05 +0000

    add unit tests for stories

--- features/step_definitions/user_story_steps.rb
+++ features/step_definitions/user_story_steps.rb
@@ -2,18 +2,6 @@ Given(/^I visit "(.*?)"$/) do |url|
   visit url
 end
 
-When(/^I fill in "(.*?)" with "(.*?)"$/) do |field, value|
-  fill_in field, with: value
-end
-
-When(/^I click "(.*?)"$/) do |button_name|
-  click_on button_name
-end
-
 Then(/^there (is|are) (\d+) stor(.*)$/) do |is_are, the_number, junk|
   Story.count.should eq(the_number.to_i)
-end
-
-Then(/^I should see "(.*?)"$/) do |message|
-  page.should have_content(message)
 end

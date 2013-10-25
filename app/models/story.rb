e01fd7c2b29c84d61d6a37600ce7252b4cb92cd3
Nathan Wiebe & Vyki Englert <pair+nwiebe+vykster@pivotallabs.com>
2013-10-25 00:15:19 +0000

    add status to story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,5 @@
 class Story < ActiveRecord::Base
 
   validates_presence_of :in_order_to, :as_a, :i_want_to
+  belongs_to :status
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024170102) do
+ActiveRecord::Schema.define(version: 20131024220608) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "statuses", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -26,6 +32,7 @@ ActiveRecord::Schema.define(version: 20131024170102) do
     t.string   "i_want_to"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "status_id"
   end
 
 end
